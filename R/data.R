754bee0779b20d0893a0966856aaedbe172d4b90
Ramón Amézquita <rameviz@icloud.com>
2024-09-25 14:39:49 +0200

    Introducing `data/` directory

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Wikipedia graph data
+#'
+#' First graph at the Wikipedia page
+#'
+#' @format
+#' A data frame with 18 rows and 3 columns:
+#' \describe{
+#'   \item{v1}{First edge of the graph from v1 to v2}
+#'   \item{v2}{Second edge of the graph from v1 to v2}
+#'   \item{w}{The weight of the edge}
+#'   ...
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra's_algorithm>
+"wiki_graph"
--- tests/testthat/test_euclidean.R
+++ tests/testthat/test_euclidean.R
@@ -1,5 +1,3 @@
-context("euclidean")
-
 test_that("GDC is calculated correctly.", {
   expect_equal(euclidean(123612, 13892347912), 4)
   expect_equal(euclidean(100, 1000), 100)
@@ -8,8 +6,8 @@ test_that("GDC is calculated correctly.", {
 
 
 test_that("Wrong input throws an error.", {
-  expect_error(euclidean("100", 1000))  
+  expect_error(euclidean("100", 1000))
   expect_error(euclidean(100, "1000"))
-  expect_error(euclidean(TRUE, "1000"))  
+  expect_error(euclidean(TRUE, "1000"))
 })
 
